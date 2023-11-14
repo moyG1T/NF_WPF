4bbf102a1676e7287497349dfd791b24f5270b0a
moy <mo9.no4ta.dz@gmail.com>
2023-11-14 12:04:08 +0300

    Пельменные бд и админки

--- NF_WPF/App.xaml.cs
+++ NF_WPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NF_WPF.Data;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,7 @@ namespace NF_WPF
     /// </summary>
     public partial class App : Application
     {
+        NF_WPF_Entities db = new NF_WPF_Entities();
+        bool isAdmin = false;
     }
 }
